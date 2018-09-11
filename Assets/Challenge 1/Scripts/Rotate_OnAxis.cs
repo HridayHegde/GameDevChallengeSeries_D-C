2d3fe932bdab1745211e0ca66a9557b19c8af72b
HridayHegde <hridayhegde1999@gmail.com>
2018-09-11 18:00:15 +0530

    Initial commit

---
+++ Assets/Challenge 1/Scripts/Rotate_OnAxis.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_OnAxis : MonoBehaviour {
+   
+    public float Speed = 50;//Challenge 1 Code
+    public float height = 0.5f;
+    public float UpDSpeed = 1;
+    // Use this for initialization
+    void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.up * Time.deltaTime * Speed,Space.World);//Challenge 1 Code
+
+        
+        Vector3 pos = transform.position;
+        
+        float newY = Mathf.Sin(Time.time * UpDSpeed);
+    
+        transform.position = new Vector3(pos.x, newY + 3.5f, pos.z) * height;
+    }
+}
